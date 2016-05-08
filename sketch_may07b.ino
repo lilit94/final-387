97930ff696d9108eb09c43a4e50ae79e60f2f218
lilit94 <tang_shiyao@me.com>
2016-05-08 00:56:39 -0400

    Added files via upload

---
+++ sketch_may07b.ino
@@ -0,0 +1,30 @@
+include <AccelStepper.h>
+#include <MultiStepper.h>
+
+#include <SPI.h>
+#include <WiFi101.h>
+
+char ssid[] = "wifiNetwork"; 
+char pass[] = "wifiPassword"; 
+int keyIndex = 0; 
+
+int status = WL_IDLE_STATUS;
+
+WiFiServer server(80);
+
+AccelStepper stepper(1, 9, 8);
+
+float distance = 0;
+float distance_mm_multiplier = 25.4;
+float duration = 0;
+float duration_sec_multiplier = 60;
+
+void setup() {
+  setup_stepper();
+  setup_wifi();
+}
+
+void loop() {
+  stepper.runSpeedToPosition();
+  WebListenForClients();
+}
